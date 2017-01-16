8a935309c9abff8ef23b36bcd5973457ef00e619
dd15015 <daaaaaavis@gmail.com>
2017-01-16 23:41:19 +0200

    initial web project

---
+++ app/Http/Controllers/userscontroller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class userscontroller extends Controller
+{
+	public function index(){
+
+	}
+
+	public function create(){
+		return view('admin.users.create');
+	}
+
+	public function store(Request $Request){
+		User::create($Request->all());
+		return 'sucess';
+		return $Request->all();
+	}
+}
